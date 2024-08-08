af2dfe396bd43518339bef5c5ee93163ad1abd62
Swagtiago29 <sant29ramos@gmail.com>
2024-08-08 16:08:36 +0000

    task 1 & sort.h

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorting bubble style
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, o, boo = 0;
+
+	if (!array || !size)
+		return;
+	while(boo < size)
+	{
+		boo++;
+		for (i = 0; i < size - 1; i++)
+		{
+			j = i + 1;
+			if (array[i] > array[j])
+			{	
+				o = array[i];
+       				array[i] = array[j];
+				array[j] = o;
+				print_array(array, size);
+			}
+		}
+	}
+}
